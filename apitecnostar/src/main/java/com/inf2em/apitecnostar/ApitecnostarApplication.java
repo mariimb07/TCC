52fca48326635065cc82cbc5f24bc24cd4228ec8
Rafaella Ferraz Fernandes <rm99561@estudante.fieb.edu.br>
2026-06-25 08:26:30 -0300

    criação do projeto back end

---
+++ apitecnostar/src/main/java/com/inf2em/apitecnostar/ApitecnostarApplication.java
@@ -0,0 +1,13 @@
+package com.inf2em.apitecnostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitecnostarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitecnostarApplication.class, args);
+	}
+
+}
---
+++ apitecnostar/src/test/java/com/inf2em/apitecnostar/ApitecnostarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inf2em.apitecnostar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitecnostarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
